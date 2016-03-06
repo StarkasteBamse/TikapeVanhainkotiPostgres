0231cf44c28cbf21d411499c468e4ca51ccdf196
Ville Saarinen <saaville@dhcp-asv-75.helsinki.fi>
2016-03-06 11:57:21 +0200

    KetjuDAO nyt testattu ja pitaisi toimia

--- TikapeVanhainkoti/src/main/java/sovelluslogiikka/Database.java
+++ TikapeVanhainkoti/src/main/java/sovelluslogiikka/Database.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class Database {
 
     private String databaseAddress;
-
+    
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;       
     }
