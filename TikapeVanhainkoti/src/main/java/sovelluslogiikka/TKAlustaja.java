08312958e1784a8ee1e39ecefc178ab434054760
Sulevi Sihvola <sulevi.sihvola@gmail.com>
2016-03-11 17:01:02 +0200

    index

--- TikapeVanhainkoti/src/main/java/sovelluslogiikka/TKAlustaja.java
+++ TikapeVanhainkoti/src/main/java/sovelluslogiikka/TKAlustaja.java
@@ -3,54 +3,56 @@ package sovelluslogiikka;
 import java.sql.*;
 
 public class TKAlustaja {
-    
+
     private Database database;
     private boolean debug = false;
 
     public TKAlustaja(Database database) {
-        this.database = database;        
+        this.database = database;
     }
 
     public void setDebug(boolean debug) {
         this.debug = debug;
     }
-    
+
     public void kokeileYhteys() {
-            try{
-                Connection connection = database.getConnection();
-                connection.close();
-            }             
-            catch (SQLException e){
-                if (debug) {
-                    System.out.println(e);
-                }
-            }                       
+        try {
+            Connection connection = database.getConnection();
+            connection.close();
+        } catch (SQLException e) {
+            if (debug) {
+                System.out.println(e);
+            }
+        }
     }
-    
-    public void luoTaulut() {    
-        
+
+    public void luoTaulut() {
+
         //taulu "Alue" luonti
-        taulunLuominen("CREATE TABLE Alue"
-                    + "(id integer PRIMARY KEY AUTOINCREMENT,"
-                    + "nimi varchar(100) NOT NULL UNIQUE)");
+        taulunLuominen("CREATE TABLE Alue "
+                + "(id integer PRIMARY KEY AUTOINCREMENT, "
+                + "nimi varchar(100) NOT NULL UNIQUE)");
         //taulu "Ketju" luonti
-        taulunLuominen("CREATE TABLE Ketju"
-                    + "(id integer PRIMARY KEY AUTOINCREMENT,"
-                    + "nimi varchar(100) NOT NULL,"
-                    + "alueId integer NOT NULL,"
-                    + "FOREIGN KEY (AlueId) REFERENCES Alue(Id))");
-         //taulu "Viesti" luonti, Nimimerkin kanssa.
-        taulunLuominen("CREATE TABLE Viesti"
-                    + "(id integer PRIMARY KEY AUTOINCREMENT,"
-                    + "viesti text NOT NULL,"
-                    + "nimimerkki varchar(50) NOT NULL, "
-                    + "pvm datetime NOT NULL,"
-                    + "ketjuId integer NOT NULL,"
-                    + "FOREIGN KEY (KetjuId) REFERENCES Ketju(Id))");   
+        taulunLuominen("CREATE TABLE Ketju "
+                + "(id integer PRIMARY KEY AUTOINCREMENT, "
+                + "nimi varchar(100) NOT NULL, "
+                + "alueId integer NOT NULL, "
+                + "FOREIGN KEY (AlueId) REFERENCES Alue(Id))");
+        //taulu "Viesti" luonti, Nimimerkin kanssa.
+        taulunLuominen("CREATE TABLE Viesti "
+                + "(id integer PRIMARY KEY AUTOINCREMENT, "
+                + "viesti text NOT NULL, "
+                + "nimimerkki varchar(50) NOT NULL, "
+                + "pvm datetime NOT NULL, "
+                + "ketjuId integer NOT NULL, "
+                + "FOREIGN KEY (KetjuId) REFERENCES Ketju(Id))");
+        taulunLuominen("CREATE INDEX idx_ketjuId ON Ketju (id), "
+                + "idx_viestiId ON Viesti (id)");
+        
     }
 
     private void taulunLuominen(String s) {
-        
+
         try {
             Connection connection = database.getConnection();
             Statement stmt = connection.createStatement();
@@ -64,5 +66,5 @@ public class TKAlustaja {
             }
         }
     }
-    
+
 }
