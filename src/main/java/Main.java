4d9e8d43183fe7f2542d00af7fa89dcfe03e7176
StarkasteBamse <sulevi.sihvola@gmail.com>
2016-03-12 15:50:37 +0200

    uuten hakemistoon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
             port(Integer.valueOf(System.getenv("PORT")));
         }
 
-        String jdbcOsoite = "jdbc:sqlite:koe.db";        
+        String jdbcOsoite = jdbcOsoite = System.getenv("DATABASE_URL");      
         if (System.getenv("DATABASE_URL") != null) {
             jdbcOsoite = System.getenv("DATABASE_URL");
         }
